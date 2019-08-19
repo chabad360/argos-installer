389a0e660c2213ae6bd31299c80146790e2bbe33
Mendel Greenberg <chabad360@gmail.com>
2019-08-18 18:17:26 -0700

    Fixed import and link issues

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-wget https://gist.githubusercontent.com/chabad360/abc4d0d60a4ed177c37fd5b68dca9887/raw/add-argos -O /usr/bin/add-argos
+wget https://raw.githubusercontent.com/chabad360/argos-installer/master/add-argos -O /usr/bin/add-argos
 chmod 655 /usr/bin/add-argos
 
-wget https://gist.githubusercontent.com/chabad360/abc4d0d60a4ed177c37fd5b68dca9887/raw/argos.desktop -O /usr/share/applications/argos.desktop
+wget https://raw.githubusercontent.com/chabad360/argos-installer/master/argos.desktop -O /usr/share/applications/argos.desktop
 
 sleep 5s
 
