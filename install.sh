a079df7a77e1acbeb49d3862efac93b7aa2074b2
chabad360 <chabad360@users.noreply.github.com>
2019-10-10 02:29:39 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-set -x
+sleep 1s
 
-sleep 1
-
+echo -n "Downloading"
 wget https://raw.githubusercontent.com/chabad360/argos-installer/master/add-argos -O /usr/bin/add-argos
+echo -n "."
 chmod 755 /usr/bin/add-argos
 
+echo -n "."
 wget https://raw.githubusercontent.com/chabad360/argos-installer/master/argos.desktop -O /usr/share/applications/argos.desktop
+echo "."
 chmod 644 /usr/share/applications/argos.desktop
 
 sleep 5s
 
+echo "Installing"
 xdg-mime default argos.desktop x-scheme-handler/bitbar
 
-set +x
+sleep 2s
+
+echo "Done!"
